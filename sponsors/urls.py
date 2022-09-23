79e848c5ca68de623418c33544e57b5e4ce71aec
Oybek Yuldashev <oybekyuldashov54@gmail.com>
2022-09-23 19:45:07 +0500

    ''

--- sponsors/urls.py
+++ sponsors/urls.py
@@ -5,7 +5,6 @@ from . import routers, views
 
 
 urlpatterns = [
-    path('<str:year>/<str:month>/', views.FilterSponsorsInDateAPIView.as_view()),
     path('count/', views.CountSponsors.as_view()),
     path('', include(routers.SPONSOR_CRUD_ROUTER.urls)),
 ]
--- sponsors/views.py
+++ sponsors/views.py
@@ -39,27 +39,3 @@ class CountSponsors(views.APIView):
     def get(self, request, *args, **kwargs):
         cnt = models.Sponsor.objects.all().count()
         return response.Response({'count': cnt}, status=status.HTTP_200_OK)    
-
-
-
-
-class FilterSponsorsInDateAPIView(views.APIView):
-
-
-    def get(self, request, *args, **kwargs):
-        year = kwargs.get('year')
-        month = kwargs.get('month')
-
-        if len(year) != 4 or len(month) != 2:
-            return response.Response(data={'year or month': 'length error'})
-
-        else:
-            try:
-                year = int(year)
-                month = int(month)
-                result = models.Sponsor.objects.filter(date_created__year=year, date_created__month=month).values()
-                return response.Response(data={'sponsors': list(result)})
-            except:
-                return response.Response(data={'year or month': 'None type int'})
-        
-
